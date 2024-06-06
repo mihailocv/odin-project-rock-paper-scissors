42f04adc39d400ecb9b4fb286ed0d670b973059f
mihailocv <127780035+mihailocv@users.noreply.github.com>
2024-06-06 15:19:39 +0200

    V1.0 UI

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const buttons = document.querySelectorAll(".button");
 const opcije = document.querySelector("#opcije");
-const poruka = document.querySelector('#poruka');
-let porukaParagraf = document.createElement('p');
+
+const poruka = document.querySelector("#poruka");
+let porukaParagraf = document.createElement("p");
 poruka.appendChild(porukaParagraf);
+
+const humanScoreNum = document.querySelector("#human-score");
+const computerScoreNum = document.querySelector("#computer-score");
+const rezultat = document.querySelector('#rezultat');
+const final = document.querySelector('#final');
+
+let humanResult = 0;
+let computerResult = 0;
 
 function getComputerChoise() {
   let broj = Math.floor(Math.random() * 3);
@@ -16,24 +25,30 @@ function getComputerChoise() {
 }
 
 function getHumanChoise(event) {
-  let target = event.target
-  return target.value;
+  return event.target.value;
 }
 
 function playRound(human, computer) {
   let result = "";
+  let finalResult ='';
   if (human === "kamen" && computer === "makaze") {
     result = "Pobjeda! Kamen je jaci od makaza!";
+    humanResult++;
   } else if (human === "papir" && computer === "kamen") {
     result = "Pobjeda! Papir je jaci od kamena!";
+    humanResult++;
   } else if (human === "makaze" && computer === "papir") {
     result = "Pobjeda! Makaze su jace od papira!";
+    humanResult++;
   } else if (human === "makaze" && computer === "kamen") {
     result = "Izgubili ste! Kamen je jaci od makaza!";
+    computerResult++;
   } else if (human === "kamen" && computer === "papir") {
     result = "Izgubili ste! Papir je jaci od kamena!";
+    computerResult++;
   } else if (human === "papir" && computer === "makaze") {
     result = "Izgubili ste! Makaze su jace od papira!";
+    computerResult++;
   } else if (
     (human === "kamen" && computer === "kamen") ||
     (human === "papir" && computer === "papir") ||
@@ -43,54 +58,24 @@ function playRound(human, computer) {
   } else {
     result = "Doslo je do greske! Pokusajte ponovo!";
   }
-
-  return result;
+  porukaParagraf.textContent = result;
+  if(humanResult === 5){
+    finalResult = "POBJEDA! CESTITAMO!";
+    humanResult = 0;
+    computerResult = 0;
+    final.textContent = finalResult;
+  } else if (computerResult === 5) {
+    finalResult = "Izgubili ste :(";
+    humanResult = 0;
+    computerResult = 0;
+    final.textContent = finalResult;
+  }
+    humanScoreNum.textContent = humanResult;
+    computerScoreNum.textContent = computerResult;
 }
 
-opcije.addEventListener('click', function(e){
-  let comp = getComputerChoise();
-  let human = getHumanChoise(e);
-
-  porukaParagraf.textContent = playRound(human, comp);
-
-  console.log(playRound(human, comp));
-})
-
-// function playGame() {
-//   let humanScore = 0;
-//   let computerScore = 0;
-
-//   do {
-//     let humanChoise = getHumanChoise();
-//     let computerChoise = getComputerChoise();
-//     let rezultat = playRound(humanChoise, computerChoise);
-//     console.log(rezultat);
-//     if (
-//       rezultat == "Pobjeda! Kamen je jaci od makaza!" ||
-//       rezultat == "Pobjeda! Papir je jaci od kamena!" ||
-//       rezultat == "Pobjeda! Makaze su jace od papira!"
-//     ) {
-//       humanScore++;
-//     } else if (
-//       rezultat == "Izgubili ste! Kamen je jaci od makaza!" ||
-//       rezultat == "Izgubili ste! Papir je jaci od kamena!" ||
-//       rezultat == "Izgubili ste! Makaze su jace od papira!"
-//     ) {
-//       computerScore++;
-//     }
-//     console.log();
-//     console.log(`Rezultat: ${humanScore}:${computerScore}`);
-//   } while (humanScore < 5 && computerScore < 5);
-
-//   if(humanScore === 5){
-//     console.log();
-//     console.log("CESTITAM! POBJEDILI STE!!!");
-//   } else {
-//     console.log();
-//     console.log("Na zalost izgubili ste :(");
-//   }
-// }
-
-
-
-// playGame();
+opcije.addEventListener("click", function (e) {
+   let comp = getComputerChoise();
+   let human = getHumanChoise(e);
+   playRound(human, comp);
+});
